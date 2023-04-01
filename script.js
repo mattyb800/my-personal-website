70de9f043553f004af04223f9b5a1b17902f52e3
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-01 14:57:12 -0400

    pic clickable

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "Learning to Code";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('pic');
+input.addEventListener('click', function() {
+    alert('This is Bubs!');
+})
